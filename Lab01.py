25addd9c830474e0ba43c6dc83cd5f088dbf6434
Shrrayashwinz <sshrrayash@gmail.com>
2025-09-02 16:53:46 -0400

    Create Lab01.py

---
+++ Lab01.py
@@ -0,0 +1,11 @@
+In Python coding, we have to use variables to store data that can be referenced and used when executing a program. A variable is a name that is assigned to a value. Python variables don’t need an explicit declaration of type, and the type of the variable is inferred based on the value assigned. Variables serve as placeholders for data. 
+
+To use the variables effectively, we need to follow Python’s naming rules. The first rule is that variable names can only have letters, digits, numbers, and underscores (_). Next, the variable name cannot start with a number/digit. In addition, the variable names are case-sensitive. Finally, you should avoid using Python Keywords (such as but not limited to if, else, for) as variable names. 
+
+In terms of using variables, there are some methods of doing so. In terms of Basic Assignment, variables are assigned to the a  = operator.   For example, x = 5, name = “Shrrayash”.
+
+Python values can also be dynamically typed, which means one variable can hold different values during execution. For example, x = 10 and x = “Class_Rocks”.
+
+In addition, you can assign the same value to many variables in a single line. For example, a = b = c = 100. 
+
+We can also assign different values to different variables simultaneously, which makes the code much easier to read. 
